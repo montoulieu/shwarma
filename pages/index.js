3a99482700d52927f11f1dad810c8fc7b82b84af
Jackson Kaufman <jacksonkaufman115@gmail.com>
2020-12-21 16:44:18 -0700

    Added social links component

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from 'next/head';
-import HomeNav from '../components/HomeNav';
+import HomeNav from '@components/HomeNav';
+import SocialLinks from '@components/SocialLinks';
 
 export default function Home() {
   return (
@@ -13,6 +14,7 @@ export default function Home() {
       </Head>
       <main className="h-screen flex items-center justify-center">
         {/* <HomeNav /> */}
+        <SocialLinks />
       </main>
     </div>
   );
