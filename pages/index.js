4de8926f642f70f028fc3156278e351c7c884f6a
Pieter Montoulieu <pieter@montoulieu.dev>
2020-12-21 15:35:17 -0700

    Updating packages, hiding nav

--- pages/index.js
+++ pages/index.js
@@ -12,7 +12,7 @@ export default function Home() {
         />
       </Head>
       <main className="h-screen flex items-center justify-center">
-        <HomeNav />
+        {/* <HomeNav /> */}
       </main>
     </div>
   );
