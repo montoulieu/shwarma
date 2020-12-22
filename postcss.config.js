3288ddc61a4027644bc7cf3f95a8a12ecb8d7dc3
Pieter Montoulieu <pieter@montoulieu.dev>
2020-12-21 22:49:45 -0700

    Post css updates for netlify

--- postcss.config.js
+++ postcss.config.js
@@ -2,7 +2,6 @@ module.exports = {
   plugins: [
     'postcss-import',
     'tailwindcss',
-    ...(process.env.NODE_ENV === 'production' ? ['autoprefixer'] : []),
     'postcss-nested', // or require('postcss-nesting')
   ],
 };
