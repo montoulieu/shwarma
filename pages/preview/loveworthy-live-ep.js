6a5e15ced127680fbf1f313512d20ec23cd75602
Pieter Montoulieu <pieter@montoulieu.dev>
2021-01-17 00:30:43 -0700

    Preview update

--- pages/preview/loveworthy-live-ep.js
+++ pages/preview/loveworthy-live-ep.js
@@ -7,7 +7,7 @@ function LoveworthyLiveEP() {
       <iframe
         style={{ border: 0, width: 400, height: 671 }}
         title="loveworthy-live-ep-bandcamp-preview"
-        src="https://bandcamp.com/EmbeddedPlayer/album=1218904325/size=large/bgcol=ffffff/linkcol=0687f5/transparent=true/tracklist=true/tracks=1427899762,3471284504,3521488986/esig=1bba86edb3e183a905219e3ef2db0cdd/"
+        src="https://bandcamp.com/EmbeddedPlayer/album=1218904325/size=large/bgcol=ffffff/linkcol=0687f5/transparent=true/tracklist=true/tracks=1427899762,3471284504,3521488986,2069037788,4158792750/esig=1f6a9b614a2f69c12676028e3de50cea/"
         className="mx-auto my-5"
         seamless
       >
