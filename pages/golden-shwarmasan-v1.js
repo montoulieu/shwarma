f478c34b2c6a4246f463eaf5c4be4e86e85bbcff
Pieter Montoulieu <pieter@montoulieu.dev>
2023-02-18 13:39:15 -0700

    Adding golden shwarmasan page

---
+++ pages/golden-shwarmasan-v1.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+const GoldenShwarmasanV1 = () => {
+  return (
+    <div className="min-h-screen flex items-center justify-center px-6">
+      <div className="text-center text-yellow-300 max-w-sm">
+        <img
+          src="/images/logo - gold.png"
+          alt="shwarma logo"
+          className="mb-4 w-72 mx-auto"
+        />
+        <h1 className="mb-2">
+          The Golden Shwarmasan and it's knowledge has been bestowed upon you.
+        </h1>
+        <h2 className="mb-2">Keep it secret, keep it safe.</h2>
+        <h3 className="mb-10">Find out more on March 1st</h3>
+        <img
+          src="/images/cheesy.jpg"
+          alt="golden shwarmasan"
+          className="rounded-xl"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default GoldenShwarmasanV1;
