ab6bd2aa17ebe18f7bef3ae863cbccda17d65f7a
Pieter Montoulieu <pieter@montoulieu.dev>
2023-03-01 17:23:37 -0700

    Text updates

--- pages/golden-shwarmasan-v1.js
+++ pages/golden-shwarmasan-v1.js
@@ -22,21 +22,22 @@ const GoldenShwarmasanV1 = () => {
         <h2 className="mb-2">
           As I peer into the depths of my fizzy soda, I see a kaleidoscope of
           bubbles swirling and dancing, each one like a tiny universe unto
-          itself. And as I watch, I can't help but wonder: are these bubbles
-          really just bubbles, or are they portals to another dimension? Could
-          it be that time itself is trapped inside these bubbles, expanding and
-          contracting with every fizzy burst? Are we all just tiny bubbles in
-          the grand cosmic soda of existence, swirling and bubbling and popping
-          in a never-ending dance of life and death? Or perhaps time is like the
-          bubbles themselves: fleeting and ephemeral, here one moment and gone
-          the next. Maybe the past and future are just as elusive as the bubbles
-          that disappear as soon as they form, leaving only a faint memory of
-          their existence. As I ponder these strange and psychedelic thoughts, I
-          take another sip of my soda and feel the bubbles tickle my tongue. And
-          in that moment, I realize that time is just like the bubbles: it may
-          be fleeting and elusive, but it's also beautiful and full of wonder.
-          And just like the bubbles, we should savor every moment while we can,
-          before it disappears into the great unknown.
+          itself. And as I watch, I can&apos;t help but wonder: are these
+          bubbles really just bubbles, or are they portals to another dimension?
+          Could it be that time itself is trapped inside these bubbles,
+          expanding and contracting with every fizzy burst? Are we all just tiny
+          bubbles in the grand cosmic soda of existence, swirling and bubbling
+          and popping in a never-ending dance of life and death? Or perhaps time
+          is like the bubbles themselves: fleeting and ephemeral, here one
+          moment and gone the next. Maybe the past and future are just as
+          elusive as the bubbles that disappear as soon as they form, leaving
+          only a faint memory of their existence. As I ponder these strange and
+          psychedelic thoughts, I take another sip of my soda and feel the
+          bubbles tickle my tongue. And in that moment, I realize that time is
+          just like the bubbles: it may be fleeting and elusive, but it&apos;s
+          also beautiful and full of wonder. And just like the bubbles, we
+          should savor every moment while we can, before it disappears into the
+          great unknown.
         </h2>
         <h3 className="mb-10">Find out more April 1st </h3>
         {/* eslint-disable-next-line @next/next/no-img-element */}
