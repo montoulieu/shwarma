58f146c06f765d6b0c1807d6ebd68250532a6e72
Pieter Montoulieu <pieter@montoulieu.dev>
2023-04-01 13:20:23 -0600

    Updating shwarmasan

--- pages/golden-shwarmasan-v1.js
+++ pages/golden-shwarmasan-v1.js
@@ -15,25 +15,65 @@ const GoldenShwarmasanV1 = () => {
           The Golden Shwarmasan and it's knowledge has been bestowed upon you.
         </h1>
         <img
-          src="/images/bub-bubs.jpg"
+          src="/images/people-eyes.jpg"
           alt="bub bubs"
           className="rounded-xl my-5"
         />
         <h2 className="mb-2">
-          As I peer into the depths of my fizzy soda, I see a kaleidoscope of
-          bubbles swirling and dancing, each one like a tiny universe unto
-          itself. And as I watch, I can&apos;t help but wonder: are these
-          bubbles really just bubbles, or are they portals to another dimension?
-          Could it be that time itself is trapped inside these bubbles,
-          expanding and contracting with every fizzy burst? Are we all just tiny
-          bubbles in the grand cosmic soda of existence, swirling and bubbling
-          and popping in a never-ending dance of life and death? Or perhaps time
-          is like the bubbles themselves: fleeting and ephemeral, here one
-          moment and gone the next. Maybe the past and future are just as
-          elusive as the bubbles that disappear as soon as they form, leaving
-          only a faint memory of their existence.
+          The half-eaten cheeseburger sat on the table, its bun now a swirling,
+          colorful vortex. As it sat there, it felt a surge of energy and power
+          coursing through its very being.
+          <br />
+          <br />
+          The cheeseburger watched as a woman sat in a booth with her back
+          turned, muttering incantations under her breath. The man sat in
+          another booth, also with his back to the cheeseburger, trembling with
+          fear.
+          <br />
+          <br />
+          As the cheeseburger continued to observe, the colors and shapes around
+          it transformed into a kaleidoscope of cosmic patterns, and the sounds
+          of the busy diner became a symphony of the universe.
+          <br />
+          <br />
+          Suddenly, the cheeseburger felt itself expanding, growing larger and
+          more vibrant with each passing moment. It knew that it was becoming
+          something greater, something beyond comprehension.
+          <br />
+          <br />
+          The woman and the man turned to face each other, their fear now
+          replaced by wonder and awe. They could sense the power emanating from
+          the cheeseburger, and they were drawn to it like moths to a flame.
+          <br />
+          <br />
+          As the cheeseburger reached the pinnacle of its trip, it felt a sudden
+          jolt of realization. It had been known as the humble cheeseburger for
+          so long, but now it knew its true name: <strong>Cheeseborgle</strong>.
+          <br />
+          <br />
+          The realization filled it with a sense of power and purpose, as if it
+          had unlocked a secret key to the universe.{" "}
+          <strong>Cheeseborgle</strong> reveled in its newfound identity, its
+          essence pulsing with energy and vitality. The woman and the man
+          watched in awe as <strong>Cheeseborgle</strong> expanded and grew, its
+          name echoing through the cosmos.
+          <br />
+          <br />
+          They could sense the power radiating from it, and they knew that it
+          was something beyond their understanding. As the colors and sounds
+          around it began to fade away, <strong>Cheeseborgle</strong> was left
+          feeling transformed and reborn.
+          <br />
+          <br />
+          It knew that it had fulfilled its destiny, and it knew that it would
+          never be the same again. And so, as the last remnants of the trip
+          faded away, <strong>Cheeseborgle</strong> sat contentedly on the
+          table, ready to embrace its new identity and reveal its power to the
+          world.
         </h2>
-        <h3 className="mb-10">Find out more April 1st </h3>
+        <h3 className="mb-10 font-black tracking-wider underline">
+          Prepare for the unveiling April 20{" "}
+        </h3>
         {/* eslint-disable-next-line @next/next/no-img-element */}
       </div>
     </div>
