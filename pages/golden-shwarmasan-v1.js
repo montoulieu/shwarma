ae1d29a7e90b89243a709050bf4be2817ae2d9be
Pieter Montoulieu <pieter@montoulieu.dev>
2023-04-20 11:45:37 -0600

    Last update for the golden shwarmasan

--- pages/golden-shwarmasan-v1.js
+++ pages/golden-shwarmasan-v1.js
@@ -15,65 +15,21 @@ const GoldenShwarmasanV1 = () => {
           The Golden Shwarmasan and it's knowledge has been bestowed upon you.
         </h1>
         <img
-          src="/images/people-eyes.jpg"
-          alt="bub bubs"
+          src="https://f4.bcbits.com/img/a2255511248_10.jpg"
+          alt="borgles"
           className="rounded-xl my-5"
         />
-        <h2 className="mb-2">
-          The half-eaten cheeseburger sat on the table, its bun now a swirling,
-          colorful vortex. As it sat there, it felt a surge of energy and power
-          coursing through its very being.
-          <br />
-          <br />
-          The cheeseburger watched as a woman sat in a booth with her back
-          turned, muttering incantations under her breath. The man sat in
-          another booth, also with his back to the cheeseburger, trembling with
-          fear.
-          <br />
-          <br />
-          As the cheeseburger continued to observe, the colors and shapes around
-          it transformed into a kaleidoscope of cosmic patterns, and the sounds
-          of the busy diner became a symphony of the universe.
-          <br />
-          <br />
-          Suddenly, the cheeseburger felt itself expanding, growing larger and
-          more vibrant with each passing moment. It knew that it was becoming
-          something greater, something beyond comprehension.
-          <br />
-          <br />
-          The woman and the man turned to face each other, their fear now
-          replaced by wonder and awe. They could sense the power emanating from
-          the cheeseburger, and they were drawn to it like moths to a flame.
-          <br />
-          <br />
-          As the cheeseburger reached the pinnacle of its trip, it felt a sudden
-          jolt of realization. It had been known as the humble cheeseburger for
-          so long, but now it knew its true name: <strong>Cheeseborgle</strong>.
-          <br />
-          <br />
-          The realization filled it with a sense of power and purpose, as if it
-          had unlocked a secret key to the universe.{" "}
-          <strong>Cheeseborgle</strong> reveled in its newfound identity, its
-          essence pulsing with energy and vitality. The woman and the man
-          watched in awe as <strong>Cheeseborgle</strong> expanded and grew, its
-          name echoing through the cosmos.
-          <br />
-          <br />
-          They could sense the power radiating from it, and they knew that it
-          was something beyond their understanding. As the colors and sounds
-          around it began to fade away, <strong>Cheeseborgle</strong> was left
-          feeling transformed and reborn.
-          <br />
-          <br />
-          It knew that it had fulfilled its destiny, and it knew that it would
-          never be the same again. And so, as the last remnants of the trip
-          faded away, <strong>Cheeseborgle</strong> sat contentedly on the
-          table, ready to embrace its new identity and reveal its power to the
-          world.
-        </h2>
-        <h3 className="mb-10 font-black tracking-wider underline">
-          Prepare for the unveiling April 20{" "}
-        </h3>
+        <p className="text-2xl font-bold">
+          Prepare for our next release, Cheeseborgle EP!
+        </p>
+        <p className="text-2xl">Out 5/29/23!</p>
+        <p className="text-xl">
+          Thanks for your support! Send a shipping address to{" "}
+          <a href="mailto:shwarmaband@gmail.com" className="underline">
+            our email
+          </a>{" "}
+          for a free copy of the CD!
+        </p>
         {/* eslint-disable-next-line @next/next/no-img-element */}
       </div>
     </div>
