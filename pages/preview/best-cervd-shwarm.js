7ce2aabd208f5de21d2490d6e47a11ac87a0ecdd
Pieter Montoulieu <pieter@montoulieu.dev>
2024-05-16 22:56:58 -0600

    feat: soundcloud embed BestCervdShwarm component

--- pages/preview/best-cervd-shwarm.js
+++ pages/preview/best-cervd-shwarm.js
@@ -1,23 +1,26 @@
+import Image from "next/image";
 import Link from "next/link";
 
 function BestCervdShwarm() {
   return (
-    <div className="min-h-screen flex flex-col content-center justify-center text-center text-white p-3">
+    <div className="flex flex-col content-center justify-center text-center text-white p-3">
       <h1 className="font-heading font-black heading text-5xl">
         Best Cerv&#39;d Shwarm
       </h1>
+      <Image
+        src="https://f4.bcbits.com/img/a0604781870_10.jpg"
+        width="400"
+        height="400"
+        className="mx-auto block mb-8"
+      />
       <iframe
-        style={{ border: 0, width: 400, height: 671 }}
-        title="best-cervd-shwarm-bandcamp-preview"
-        // src="https://bandcamp.com/EmbeddedPlayer/album=3667111821/size=large/bgcol=181a1b/linkcol=056cc4/transparent=true/tracklist=true/tracks=3761387873,1233339987,2005690326,2039400135,3444074936,3555189002,3320950993,2839155940,3585083184,1227262305/esig=f141222ffef72830b9116a13ffb95173/"
-        src="https://bandcamp.com/EmbeddedPlayer/album=255999805/size=large/bgcol=ffffff/linkcol=0687f5/transparent=true/tracklist=true/tracks=2522972468,3599368569,1963363111,1016601004,3128043940,1129636441,2064965087,4168599141,1890657596,2176739429,381528454,936660551,3331139437,1823131244/esig=7c6b6571b6441c489f07de9a895f4a7d/"
-        className="mx-auto my-5"
-        seamless
-      >
-        <a href="https://music.shwarma.band/album/best-cervd-shwarm">
-          Best Cerv&#39;d Shwarm by shwarma
-        </a>
-      </iframe>
+        className="max-w-md w-full mx-auto block mb-8"
+        width="100%"
+        height="600"
+        frameborder="no"
+        allow="autoplay"
+        src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/1824337704%3Fsecret_token%3Ds-smgPCnWsZdy&color=%23ff5500&auto_play=false&hide_related=true&show_comments=false&show_user=false&show_reposts=false&show_teaser=true&visual=false&show_artwork=false"
+      ></iframe>
       <Link href="/" className="font-heading text-2xl btn mx-auto inline">
         Back to the shwarmth
       </Link>
