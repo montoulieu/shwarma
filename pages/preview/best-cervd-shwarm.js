bcaea741af721d0c032254c80ff00ee6f7372739
Pieter Montoulieu <pieter@montoulieu.dev>
2024-05-18 22:07:46 -0600

    feat: Add NextSeo component and update page layout for BestCervdShwarm

--- pages/preview/best-cervd-shwarm.js
+++ pages/preview/best-cervd-shwarm.js
@@ -1,18 +1,37 @@
+import { NextSeo } from "next-seo";
 import Image from "next/image";
 import Link from "next/link";
 
 function BestCervdShwarm() {
   return (
-    <div className="max-w-md mx-auto flex flex-col content-center justify-center text-center p-3">
-      <h1 className="font-heading font-black heading text-5xl">
-        Best Cerv&#39;d Shwarm
-      </h1>
+    <div className="max-w-md mx-auto flex flex-col content-center justify-center text-center px-3 py-4">
+      <NextSeo
+        title="Best Cerv'd Shwarm - Album Preview"
+        description="Best Cerv'd Shwarm is a live album compilation drawing from 3 shows performed at Cervantes last year."
+        openGraph={{
+          images: [
+            {
+              url: "/images/album/best-cervd-shwarm-md-compress.jpg",
+            },
+          ],
+        }}
+      />
+
+      <header className="mb-4">
+        <h1 className="text-brand font-bold text-3xl tracking-wider uppercase">
+          Best Cerv&#39;d Shwarm
+        </h1>
+        <h2 className="text-brand text-xl">Album Preview</h2>
+      </header>
+
       <Image
-        src="https://f4.bcbits.com/img/a0604781870_10.jpg"
+        src="/images/album/best-cervd-shwarm-md-compress.jpg"
         width="400"
         height="400"
         className="mx-auto block mb-8"
+        alt="Best Cerv'd Shwarm album cover"
       />
+
       <div className="text-brand text-sm mb-6">
         <p>
           Best Cerv&#39;d Shwarm is a live album compilation drawing from 3
@@ -27,6 +46,7 @@ function BestCervdShwarm() {
           Catcher and Kaepora. It&#39;s gonna be kick-ass, so come on out!
         </p>
       </div>
+
       <iframe
         className="w-full mx-auto block mb-8"
         width="100%"
@@ -34,7 +54,8 @@ function BestCervdShwarm() {
         frameborder="no"
         allow="autoplay"
         src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/1824337704%3Fsecret_token%3Ds-smgPCnWsZdy&color=%23ff5500&auto_play=false&hide_related=true&show_comments=false&show_user=false&show_reposts=false&show_teaser=true&visual=false&show_artwork=false"
-      ></iframe>
+      />
+
       <Link href="/" className="font-heading text-2xl btn mx-auto inline">
         Back to the shwarmth
       </Link>
